3f1ac7cba881b581cac1a1df18fd7523e58e9ab2
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-03 01:50:26 -0700

    Add files via upload

---
+++ Sorting/Overlapping Intervals.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeOverlap(self, arr):
+        arr.sort(key=lambda x: x[0])
+        merged = []
+
+        for interval in arr:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
