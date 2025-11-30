b92bd27e39ebb74aba0432b3263143b319522e39
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-30 11:51:01 -0500

    Delete Heap directory

--- Heap/K Closest Points to Origin.py
+++
@@ -1,5 +0,0 @@
-import heapq
-
-class Solution:
-    def kClosest(self, points, k):
-        return heapq.nsmallest(k, points, key=lambda p: p[0]**2 + p[1]**2)
--- Heap/k largest elements.py
+++
@@ -1,5 +0,0 @@
-import heapq
-
-class Solution:
-    def kLargest(self, arr, k):
-        return heapq.nlargest(k, arr)
