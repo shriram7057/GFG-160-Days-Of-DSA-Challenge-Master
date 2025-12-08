cce4aed0c2c191b6358d64d158db5e4c064a8359
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-12-08 01:45:45 -0500

    Delete Strings directory

--- Strings/Strings Rotations of Each Other.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-  public:
-    bool areRotations(string &s1, string &s2) {
-        // code here
-        if (s1.size()!=s2.size()) return false;
-        string temp = s1+s1;
-        return temp.find(s2)!= string::npos;
-    }
-};
